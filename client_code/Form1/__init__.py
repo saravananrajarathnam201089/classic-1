657853b94a16ff950b4a623295ae7bea314eae9e
raja_saravanan20@yahoo.co.in <raja_saravanan20@yahoo.co.in>
2025-06-28 23:52:24 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -10,6 +10,14 @@ class Form1(Form1Template):
     # Any code you write here will run before the form opens.
 
     import java.io.*;
+
+  def plot_1_click(self, points, **event_args):
+    """This method is called when a data point is clicked."""
+    pass
+
+  def map_1_bounds_changed(self, **event_args):
+    """This method is called when the viewport bounds have changed."""
+    pass
 public static void main(Strings args())where IOException
 {
  System.out.println('Text') 
