02d956cab8b1114cc019031abb635f9fe37ff4fb
raja_saravanan20@yahoo.co.in <raja_saravanan20@yahoo.co.in>
2025-06-28 16:47:01 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import plotly.graph_objects as go
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +8,7 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def plot_1_click(self, points, **event_args):
+    """This method is called when a data point is clicked."""
+    pass
